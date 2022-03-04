f63d727f182ab4f8671c8ac9d9bbb643a440c59e
iamalanxue <alanxue43@gmail.com>
2022-03-04 13:16:48 -0600

    made table of all info in part 2

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+import csv
 import texttable
 from texttable import Texttable
 import json 
@@ -6,6 +7,7 @@ import sys
 def part1():
   table = Texttable(max_width=0)
   table.set_deco(Texttable.HEADER)
+  long_columns = ['ipv4_addresses', 'ipv6_addresses', 'rdns_names','rtt_range', 'tls_versions']
   rows = []
   first_key = next(iter(data))
   columns = ['webpage']
@@ -13,13 +15,29 @@ def part1():
   for webpage in data:
     row = [webpage]
     for info in data[webpage]:
-      row.append(data[webpage][info])
+      if info in long_columns and data[webpage][info] is not None:
+        s = '\n'.join(str(e) for e in data[webpage][info])
+        row.append(s)
+      elif info == 'geo_locations':
+        csv_locations = data[webpage][info][0].split(', ')
+        dest = []
+        for i in range(0, len(csv_locations) - 2, 3):
+          s = ' '.join(csv_locations[i:i+3])
+          dest.append(s)
+        fin = '\n'.join(str(x) for x in dest)
+        row.append(fin)
+      else:
+        row.append(data[webpage][info])
     rows.append(row)
-  # table.set_cols_align(["l"]*len(columns))
-  # table.set_cols_valign(["m"]*len(columns)) this is just styling dont need this
+  table.set_cols_align(['l']*13)
+  table.set_cols_width((13, 55, 10, 15, 10,20, 20, 10, 5, 10,10, 10, 10))
+  # table.set_cols_valign(["m"]*len(columns))
   rows.insert(0, columns)
   table.add_rows(rows)
-  print(table.draw())
+  pretty_table = table.draw()
+  with open('output.txt', 'w', encoding="utf-8") as f:
+    f.write(pretty_table)
+  # print(pretty_table)
 
 user_in = sys.argv[1]
 user_out = sys.argv[2]
